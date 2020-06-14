13e5b59770b2b6bc50d9f8e1993e12ab1898e2c5
Travis Sceroler <Jsceroler@gmail.com>
2020-06-13 19:24:56 -0500

    Updates

---
+++ precache-manifest.2e3b4bd0637f82fafce1da2988d9dd55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc588d3141ce019875c599adb718a191",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "faf36e975d07616ec5d3",
+    "url": "/Clicky-Game/static/css/main.e4bcce56.chunk.css"
+  },
+  {
+    "revision": "e50d4c9609983495334d",
+    "url": "/Clicky-Game/static/js/2.26f1edb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clicky-Game/static/js/2.26f1edb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faf36e975d07616ec5d3",
+    "url": "/Clicky-Game/static/js/main.ea093714.chunk.js"
+  },
+  {
+    "revision": "28943688ad0891451581",
+    "url": "/Clicky-Game/static/js/runtime-main.325a4581.js"
+  }
+]);
